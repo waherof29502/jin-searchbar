696b6ed827e704571959267d4fd8b40d39839725
JinPing <39645652+waherof29502@users.noreply.github.com>
2022-11-18 09:45:45 +0800

    Finished Code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Selector from './components/Selector';
+
+const App = () => {
+  return (
+    <div className='bg-gray-900 font-Poppins flex h-screen items-center justify-center'>
+      <Selector />
+    </div>
+  );
+};
+
+export default App;
